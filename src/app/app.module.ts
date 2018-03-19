fb7f827422311fc419eb3a4ce606469b5c07348f
Sagar Thapa <sagarthapa_9@yahoo.com>
2018-03-19 19:16:58 +0000

    added settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HttpModule} from '@angular/http';
+import { IonicStorageModule} from '@ionic/storage';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+// import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -18,7 +19,7 @@ import { WeatherProvider } from '../providers/weather/weather';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    // ContactPage,
     SettingsPage,
     HomePage,
     TabsPage
@@ -26,13 +27,14 @@ import { WeatherProvider } from '../providers/weather/weather';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    // ContactPage,
     HomePage,
     SettingsPage,
     TabsPage
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider} from '../../providers/weather/weather';
+import { Storage} from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,20 +14,30 @@ export class HomePage {
   	state: string;
   }
 
-  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+  constructor(public navCtrl: NavController,
+   private weatherProvider: WeatherProvider,
+   private storage: Storage) {
+
 
   }
 
    ionViewWillEnter(){
-   	this.location={
-   		city: 'Oxford',
-   		state: 'UK'
-   	}
+   	this.storage.get('location').then((val) => {
+          if(val != null){
+              this.location = JSON.parse(val);
+          }else{
+             this.location = {
+             		city: 'Oxford',
+   					state: 'UK'
+             }
+          }
 
-   	this.weatherProvider.getWeather(this.location.city, this.location.state)
-   	.subscribe(weather => {
+ 	this.weatherProvider.getWeather(this.location.city, this.location.state)
+   		.subscribe(weather => {
    		this.weather = weather.current_observation;
-   		console.log(this.weather);
+   		});
    	});
+
+
    }
 }
